a28535d136940fb03370c21a3f98acfc03f542e2
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2022-11-07 15:18:28 -0300

    Clear cache

--- public/cap-04/app/app.js
+++ public/cap-04/app/app.js
@@ -1,10 +1,39 @@
 import { memoiser } from "./infra/memoiser.js";
 
 function somaDoisNumeros(num1, num2) {
+  console.log('Entrou soma dois numeros');
   return num1 + num2;
 }
 
 const somaDoisNumerosM = memoiser(somaDoisNumeros);
-console.log(somaDoisNumeros(5, 5));
-console.log(somaDoisNumeros(10, 10));
 console.log(somaDoisNumerosM(5, 5));
+console.log(somaDoisNumerosM(10, 10));
+console.log(somaDoisNumerosM(5, 5));
+
+const fatorial =  memoiser(n => {
+  if (n <= 1) return 1;
+  return n * fatorial(--n);
+});
+
+
+const f1 = fatorial(4);
+const f2 = fatorial(3);
+const f3 = fatorial(2);
+console.log(f1);
+console.log(f2);
+console.log(f3);
+
+const fetchHandler = res => 
+  res.ok ? res.json() : Promise.reject(res.statusText)
+
+const getNotaFromId = id => 
+  fetch(`http://localhost:3000/notas/${id}`)
+    .then(fetchHandler);
+
+const getNotaFromIdM = memoiser(getNotaFromId);
+
+getNotaFromIdM(1)
+  .then(console.log)
+  .then(getNotaFromIdM.release())
+  .then(getNotaFromIdM(1))
+  .catch(e => console.log(e));
--- public/cap-04/app/infra/memoiser.js
+++ public/cap-04/app/infra/memoiser.js
@@ -1,6 +1,23 @@
 export function memoiser(fn) {
-  return (...args) => {
-    const result = fn(...args);
-    return result;
+  const cache = new Map();
+
+  const newFn = (...args) => {
+    const key = JSON.stringify(args);
+    if(cache.has(key)) {
+      console.log(`Buscou do cache ${key}`);
+      return cache.get(key);
+    } else {
+      console.log(`Não encontrou no cache ${key}. Adicionando no cache`);
+      const result = fn(...args);
+      cache.set(key, result)
+      return result;
+    }
   }
+
+  newFn.release = () => {
+    console.log('Limpando o cache');
+    cache.clear();
+  }
+  return newFn;
+    
 }
